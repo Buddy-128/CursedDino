80368d2645f03e09524a48afcdafdcbddcdb8933
Martin Nedoma <nedoma.martin@icloud.com>
2026-06-01 19:06:13 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-echo "Building ..."
-cc game.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -o game.o
-echo "Done! Run the game?? (Y/N)"
-read x
-if [ $x == "Y" ]; then
-   ./game.o
-fi
